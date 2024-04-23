61dda2d450f9eaf224618027c7b701c48c0ae477
Akshith Mysa <akshiyhmysa.physics@gmail.com>
2024-04-23 19:05:12 +0530

    initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html className="dark">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
