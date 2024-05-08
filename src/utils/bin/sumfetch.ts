b869fc5548ca2deb6ce4b0224ad9f2c53d5edf68
Akshith Mysa <akshiyhmysa.physics@gmail.com>
2024-05-08 14:29:21 +0530

    contact info

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -12,7 +12,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
 @@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 @@                                   @@     -----------
 @@             .@@@@@@@@@@.          @@      CONTACT
- @@           @@          @@        @@      ✆ <u><a href="tel:${config.phone}" target="_blank">+91${config.phone}</a></u>
+ @@           @@          @@        @@      ✆ <u><a href="tel:${config.phone}" target="_blank">+91 ${config.phone}</a></u>
   @@           @@        @@        @@        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u><Right>
    @@             @@@@@@          @@         <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
      @@@                        @@@          <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
